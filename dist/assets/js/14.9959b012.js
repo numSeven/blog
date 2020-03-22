bf8703ca3bd9849e5bb0204195db091fd1ecb8c3
Jameszhan <jameszhan@xiaoe-tech.com>
2020-03-22 22:51:44 +0800

    ''ss''

---
+++ dist/assets/js/14.9959b012.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{738:function(n,w,o){}}]);
--- docs/config/navsConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports  = [
-  {text: '编程技术', link: '/programe/'},
-  {text: '生活兴趣', link: '/life/'},
-  {text: '编程技术', link: '/tool/'},
-  {text: '编程技术', link: '/other/'}
-]
